15e24d263d6b28767bdbf8cd850a741b5d1f5099
zpub <lil.rhymes9@gmail.com>
2024-06-02 21:40:57 +0300

    Регистрация работает как надо

--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -1,23 +1,67 @@
 import {Container} from "@mui/material";
-
-import './Registration.css'
-import {Link} from "react-router-dom";
+import './Registration.css';
+import {Link, useNavigate} from "react-router-dom";
 import apple from "../login/images/apple.svg";
 import google from "../login/images/google.svg";
 import facebook from "../login/images/facebook.svg";
 import React, {useState} from "react";
+import axios from "axios";
 
 const Registration = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [errors, setErrors] = useState({});
+    const [serverError, setServerError] = useState('');
+
+    const navigate = useNavigate();
+
+    const validateForm = () => {
+        const errors = {};
+
+        if (!username) {
+            errors.username = "Поле 'Адреса електронної пошти або номер телефону' обов'язкове.";
+        }
+
+        if (!password) {
+            errors.password = "Поле 'Пароль' обов'язкове.";
+        } else if (password.length < 6) {
+            errors.password = "Пароль повинен містити мінімум 6 символів.";
+        }
+
+        return errors;
+    };
+
+    const register = (e) => {
+        e.preventDefault();
+
+        const formErrors = validateForm();
+        if (Object.keys(formErrors).length > 0) {
+            setErrors(formErrors);
+            return;
+        }
+
+        axios.post("http://localhost:8080/api/auth/registration", {
+            username: username,
+            password: password
+        })
+            .then(resp => {
+                navigate("/login");
+            })
+            .catch(error => {
+                if (error.response && error.response.data) {
+                    setServerError(error.response.data);
+                } else {
+                    setServerError("Виникла помилка. Будь ласка, спробуйте пізніше.");
+                }
+            });
+    };
 
     return (
         <Container maxWidth="lg" className="registration">
             <h1 className="title">ЗАРЕЄСТРУВАТИСЬ</h1>
 
             <div className="form-container">
-                <form onSubmit={()=>{}}>
+                <form onSubmit={register}>
                     <div className="inputs">
                         <input
                             id="username"
@@ -39,13 +83,12 @@ const Registration = () => {
                         {errors.password && <div className="error">{errors.password}</div>}
                     </div>
 
+                    {serverError && <div className="error">{serverError}</div>}
 
                     <div className="actions">
                         <button to="#" type="submit" className="login_button">Зареєструватись</button>
                         <Link to="/login" className="login_in_account">Увiйти в акаунт</Link>
                     </div>
-
-
 
                     <div className="social_container">
                         <div className="social_list">
