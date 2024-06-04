f00b761db010412dd58eb584fec0c7899240b66f
Anastasiia Kudriashova <kudriashova.ag@gmail.com>
2024-06-04 11:04:35 +0300

    themeOptions

--- src/app/loader.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { http } from "./http";
-
-export const getFilms = async () => {
-    const { data } = await http.get(
-        "/films.json"
-    );
-    return data;
-}
--- src/routes/index.js
+++ src/routes/index.js
@@ -36,7 +36,7 @@ export const router = createBrowserRouter([
             },
             {
                 path: '/movies',
-                element: <Movies />
+                element: <Movies />,
             },
             {
                 path: '/series',
