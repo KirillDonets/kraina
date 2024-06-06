3c886ae57e06afaee2b238904b9fcacd6aeae321
KirillDonets <97831177+KirillDonets@users.noreply.github.com>
2024-06-06 20:29:49 +0300

    Просмотр фильма

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,69 @@
-import React from 'react';
-import { Container, Grid, Card, CardContent, Typography, Button } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Container, Grid, Card, CardContent, Typography, Button, Box, CardMedia } from '@mui/material';
+import Slider from 'react-slick';
 import './Home.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const apiKey = '6354d9421b6c9d2510d1a693d1dc40b4';
+const token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzU0ZDk0MjFiNmM5ZDI1MTBkMWE2OTNkMWRjNDBiNCIsInN1YiI6IjY2MWUwNzRiZDc1YmQ2MDE0OTMwYjkyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RgpHSSmlqPeSbkO8Tgkva_SbS937PRPTX_4nBKsFSHI';
+const baseUrl = 'https://api.themoviedb.org/3';
 
 const Home = () => {
+    const [newReleases, setNewReleases] = useState([]);
+
+    useEffect(() => {
+        const fetchNewReleases = async () => {
+            try {
+                const response = await fetch(`${baseUrl}/movie/now_playing?api_key=${apiKey}&language=uk-UA&page=1`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        'Content-Type': 'application/json;charset=utf-8'
+                    }
+                });
+                const data = await response.json();
+                setNewReleases(data.results.slice(0, 20));
+            } catch (error) {
+                console.error('Ошибка при загрузке новинок:', error);
+            }
+        };
+
+        fetchNewReleases();
+    }, []);
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
     return (
         <Container maxWidth="lg">
             <h1>Home</h1>
@@ -13,8 +74,8 @@ const Home = () => {
                             <Typography variant="h5" component="div">
                                 Базовий
                             </Typography>
-                            <Typography variant="body2">                            
-                                120 ₴ в месяц <br /> 
+                            <Typography variant="body2">
+                                120 ₴ в месяц <br />
                                 • Якість відео та звуку <br />
                                 Нормальна <br /> <hr />
                                 • Роздільна здатність <br />
@@ -29,7 +90,6 @@ const Home = () => {
                                 2 <br /> <hr />
                                 • Реклама
                                 Кілька рекламних пауз <br />
-                                 
                             </Typography>
                         </CardContent>
                         <div className="card-button-container">
@@ -46,7 +106,7 @@ const Home = () => {
                                 Стандарт
                             </Typography>
                             <Typography variant="body2">
-                                120 ₴ в месяц <br /> 
+                                120 ₴ в месяц <br />
                                 • Якість відео та звуку <br />
                                 Нормальна <br /> <hr />
                                 • Роздільна здатність <br />
@@ -77,7 +137,7 @@ const Home = () => {
                                 Преміум
                             </Typography>
                             <Typography variant="body2">
-                                120 ₴ в месяц <br /> 
+                                120 ₴ в месяц <br />
                                 • Якість відео та звуку <br />
                                 Чудова <br /> <hr />
                                 • Роздільна здатність <br />
@@ -96,7 +156,7 @@ const Home = () => {
                         </CardContent>
                         <div className="card-button-container">
                             <Button className="card-button" variant="contained" color="primary">
-                                Отримати Премиум
+                                Отримати Преміум
                             </Button>
                         </div>
                     </Card>
@@ -104,9 +164,29 @@ const Home = () => {
                         Є питання.<br />
                         Відповідаемо<br />
                         Ще більше інформації можна знайти
-                        </div>
+                    </div>
                 </Grid>
             </Grid>
+            <Box mt={5}>
+                <Typography variant="h4" gutterBottom>Топ-20 новинок</Typography>
+                <Slider {...settings}>
+                    {newReleases.map((movie) => (
+                        <div key={movie.id}>
+                            <Card className="movie-card">
+                                <CardContent>
+                                    <CardMedia
+                                        component="img"
+                                        height="300"
+                                        image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                        title={movie.title}
+                                    />
+                                    <Typography variant="h6">{movie.title}</Typography>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    ))}
+                </Slider>
+            </Box>
         </Container>
     );
 }
--- src/pages/movieDetails/MovieDetails.jsx
+++ src/pages/movieDetails/MovieDetails.jsx
@@ -1,7 +1,7 @@
 // MovieDetails.js
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container, CircularProgress, Typography, Card, CardMedia, Box } from '@mui/material';
+import { Container, CircularProgress, Typography, CardMedia, Box } from '@mui/material';
 
 const apiKey = '6354d9421b6c9d2510d1a693d1dc40b4';
 const token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MzU0ZDk0MjFiNmM5ZDI1MTBkMWE2OTNkMWRjNDBiNCIsInN1YiI6IjY2MWUwNzRiZDc1YmQ2MDE0OTMwYjkyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RgpHSSmlqPeSbkO8Tgkva_SbS937PRPTX_4nBKsFSHI';
@@ -10,12 +10,13 @@ const baseUrl = 'https://api.themoviedb.org/3';
 const MovieDetails = () => {
     const { id } = useParams();
     const [movie, setMovie] = useState(null);
+    const [credits, setCredits] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchMovieDetails = async () => {
             try {
-                const response = await fetch(`${baseUrl}/movie/${id}?api_key=${apiKey}&language=uk-UA`, {
+                const response = await fetch(`${baseUrl}/movie/${id}?api_key=${apiKey}&language=uk-UA&append_to_response=videos`, {
                     headers: {
                         Authorization: `Bearer ${token}`,
                         'Content-Type': 'application/json;charset=utf-8'
@@ -23,10 +24,19 @@ const MovieDetails = () => {
                 });
                 const data = await response.json();
 
+                const creditsResponse = await fetch(`${baseUrl}/movie/${id}/credits?api_key=${apiKey}&language=uk-UA`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        'Content-Type': 'application/json;charset=utf-8'
+                    }
+                });
+                const creditsData = await creditsResponse.json();
+
                 setMovie(data);
+                setCredits(creditsData);
                 setLoading(false);
             } catch (error) {
-                console.error('Error fetching movie details:', error);
+                console.error('Ошибка при загрузке данных о фильме:', error);
                 setLoading(false);
             }
         };
@@ -42,10 +52,13 @@ const MovieDetails = () => {
         );
     }
 
+    const director = credits?.crew.find(person => person.job === 'Director');
+    const cast = credits?.cast.slice(0, 5).map(actor => actor.name).join(', ');
+
     return (
         <Container maxWidth="lg">
-            <Box display="flex" alignItems="center">
-                {movie && movie.poster_path && (
+            <Box display="flex" alignItems="flex-start" mb={2}>
+                {movie?.poster_path && (
                     <CardMedia
                         component="img"
                         height="500"
@@ -54,15 +67,34 @@ const MovieDetails = () => {
                     />
                 )}
                 <Box ml={2}>
-                    <Typography variant="h4" gutterBottom>{movie.title}</Typography>
-                    <Typography variant="body1">{movie.overview}</Typography>
+                    {movie?.title && <Typography variant="h4" gutterBottom>{movie.title}</Typography>}
+                    {movie?.original_title && <Typography variant="h6" gutterBottom>{movie.original_title}</Typography>}
+                    {movie?.overview && <Typography variant="body1" gutterBottom>{movie.overview}</Typography>}
+                    {movie?.vote_average && <Typography variant="body2">Рейтинги: {movie.vote_average}</Typography>}
+                    {movie?.release_date && <Typography variant="body2">Дата виходу: {movie.release_date}</Typography>}
+                    {movie?.production_countries.length > 0 && <Typography variant="body2">Країна: {movie.production_countries.map(country => country.name).join(', ')}</Typography>}
+                    {director?.name && <Typography variant="body2">Режисер: {director.name}</Typography>}
+                    {movie?.adult !== undefined && <Typography variant="body2">Вік: {movie.adult ? '18+' : 'Всі віки'}</Typography>}
+                    {movie?.runtime && <Typography variant="body2">Тривалість: {movie.runtime} хв.</Typography>}
+                    {cast && <Typography variant="body2">У ролях: {cast}</Typography>}
+                </Box>
+            </Box>
+            <Box mt={2} position="relative" paddingBottom="56.25%" height="0" overflow="hidden">
+                {movie?.videos?.results.length > 0 && (
                     <CardMedia
                         component="iframe"
-                        height="315"
-                        src={`https://www.youtube.com/watch?v=Ed-rbhWhzTU/${movie.trailer}`}
-                        title="Movie Trailer"
+                        src={`https://www.youtube.com/embed/${movie.videos.results[0].key}`}
+                        title="Трейлер фільму"
+                        allow="fullscreen"
+                        style={{
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            width: '100%',
+                            height: '100%'
+                        }}
                     />
-                </Box>
+                )}
             </Box>
         </Container>
     );
